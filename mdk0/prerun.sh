548661fbafd4e713a4a3c2b2d33f21be6d5ac425
Michael Ketchel <mdk@rgnets.com>
2024-10-24 18:03:44 -0400

    Fix prerun

--- mdk0/prerun.sh
+++ mdk0/prerun.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
-if [ ! -d "${ROOTFS_DIR}" ] || [ "${USE_QCOW2}" = "1" ]; then
-	bootstrap ${RELEASE} "${ROOTFS_DIR}" http://deb.debian.org/debian/
+if [ ! -d "${ROOTFS_DIR}" ]; then
+	copy_previous
 fi
