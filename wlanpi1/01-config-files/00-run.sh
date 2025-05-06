69d0cf5f8c7525580067c9dd8a0c0b6072e40f7b
Michael Ketchel <mdk@rgnets.com>
2025-05-06 19:46:58 -0400

    Friggin regdb

--- wlanpi1/01-config-files/00-run.sh
+++ wlanpi1/01-config-files/00-run.sh
@@ -62,10 +62,11 @@ on_chroot <<CHEOF
 	# Prevent interfaces from being managed by dhcpcd which conflicts with systemd
 	echo "denyinterfaces usb* pan*" | tee -a /etc/dhcpcd.conf
 
+  REGDB_FILE="wireless-regdb_2025.02.20-1_all.deb"
 	# Install wireless-regdb which supports Wi-Fi 6E
-	wget -O /tmp/wireless-regdb_2024.10.07-2_all.deb http://ftp.us.debian.org/debian/pool/main/w/wireless-regdb/wireless-regdb_2024.10.07-2_all.deb
-	dpkg -i /tmp/wireless-regdb_2024.10.07-2_all.deb
-	rm -f /tmp/wireless-regdb_2024.10.07-2_all.deb
+	wget -O /tmp/$REGDB_FILE http://ftp.us.debian.org/debian/pool/main/w/wireless-regdb/$REGDB_FILE
+	dpkg -i /tmp/$REGDB_FILE
+	rm -f /tmp/$REGDB_FILE
 	update-alternatives --set regulatory.db /lib/firmware/regulatory.db-upstream
 
 	# Fix sntp permission error
