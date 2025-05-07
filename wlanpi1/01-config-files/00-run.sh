56cea5440eb27a11c6d757f94de74a2ff779adbf
Michael Ketchel <mdk@rgnets.com>
2025-05-06 21:11:26 -0400

    fix regdb

--- wlanpi1/01-config-files/00-run.sh
+++ wlanpi1/01-config-files/00-run.sh
@@ -62,12 +62,12 @@ on_chroot <<CHEOF
 	# Prevent interfaces from being managed by dhcpcd which conflicts with systemd
 	echo "denyinterfaces usb* pan*" | tee -a /etc/dhcpcd.conf
 
-  REGDB_FILE="wireless-regdb_2025.02.20-1_all.deb"
-  echo "Downloading wireless regdb $REGDB_FILE"
+  #REGDB_FILE="wireless-regdb_2025.02.20-1_all.deb"
+  echo "Downloading wireless regdb wireless-regdb_2025.02.20-1_all.deb"
 	# Install wireless-regdb which supports Wi-Fi 6E
-	wget -O /tmp/$REGDB_FILE http://ftp.us.debian.org/debian/pool/main/w/wireless-regdb/$REGDB_FILE
-	dpkg -i /tmp/$REGDB_FILE
-	rm -f /tmp/$REGDB_FILE
+	wget -O /tmp/wireless-regdb_2025.02.20-1_all.deb http://ftp.us.debian.org/debian/pool/main/w/wireless-regdb/wireless-regdb_2025.02.20-1_all.deb
+	dpkg -i /tmp/wireless-regdb_2025.02.20-1_all.deb
+	rm -f /tmp/wireless-regdb_2025.02.20-1_all.deb
 	update-alternatives --set regulatory.db /lib/firmware/regulatory.db-upstream
 
 	# Fix sntp permission error
